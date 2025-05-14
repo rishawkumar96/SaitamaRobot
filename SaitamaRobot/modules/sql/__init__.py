7d38da21f10b7ba07cf682d907a23ac42d9afcc0
rishawkumar96 <rishawkumar96@gmail.com>
2025-05-14 10:22:24 +0530

    Update __init__.py

--- SaitamaRobot/modules/sql/__init__.py
+++ SaitamaRobot/modules/sql/__init__.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 
 
 def start() -> scoped_session:
-    engine = create_engine(SQLALCHEMY_DATABASE_URI)
+    engine = create_engine(DATABASE_URI, echo=False)
     BASE.metadata.bind = engine
     BASE.metadata.create_all(engine)
     return scoped_session(sessionmaker(bind=engine, autoflush=False))
