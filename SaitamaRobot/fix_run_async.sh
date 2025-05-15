2b3ff0c9c3c3dfa385f231b4fbfc55034202959c
rishawkumar96 <rishawkumar96@gmail.com>
2025-05-15 14:45:37 +0530

    Update fix_run_async.sh

--- SaitamaRobot/fix_run_async.sh
+++ SaitamaRobot/fix_run_async.sh
@@ -4,11 +4,8 @@
 find . -type f -name "*.py" -exec sed -i '/^\s*@run_async\s*$/d' {} +
 
 # Add run_async=True param to handler constructors, if not already present
-# This handles CommandHandler, MessageHandler, RegexHandler, and CustomCommandHandler
-
 find . -type f -name "*.py" | while read -r file; do
-    # Only add run_async if not already present on that line
-    sed -i -r '/(CommandHandler|MessageHandler|RegexHandler|CustomCommandHandler)\(/ {
-        /run_async\s*=/! s/(\(([^)]*))/\1, run_async=True/'
+    sed -i '/\(CommandHandler\|MessageHandler\|RegexHandler\|CustomCommandHandler\)(/ {
+        /run_async\s*=/! s/\(\(([^)]*\))/\1, run_async=True/'
     }' "$file"
 done
